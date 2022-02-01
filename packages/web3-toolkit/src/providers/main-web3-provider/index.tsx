e026928be338de46e579351a1fe99121647921dd
Elvaleryn <eraykaya121@gmail.com>
2022-02-01 18:02:07 +0300

    Eslint errors removed

--- packages/web3-toolkit/src/providers/main-web3-provider/index.tsx
+++ packages/web3-toolkit/src/providers/main-web3-provider/index.tsx
@@ -6,16 +6,13 @@ const web3Context = React.createContext<UseWeb3ContextReturnType | null>(null);
 const { Provider } = web3Context;
 
 export const MainWeb3ContextProvider: React.FC<Web3ContextProviderProps> = ({ children, networks }) => {
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
   const web3Context = useMainWeb3ContextProvider(networks);
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
   return <Provider value={web3Context}>{children}</Provider>;
 };
 
 export const useMainWeb3Context = (): {
   config: { networks: NetworkProviders };
 } | null => {
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-return
   return React.useContext(web3Context);
 };
 
